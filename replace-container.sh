a544ebb503804459c2c435aa869d8a107d77cc66
Zi Wei <wong_ziwi1996@hotmail.com>
2022-02-02 14:22:56 +0800

    Update container script

--- replace-container.sh
+++ replace-container.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-docker stop con
-docker system prune -f
-docker pull ziwei0/awsdevops
-docker run -d -p 8080:8080 --name con ziwei0/awsdevops
+if ! docker pull ziwei0/awsdevops | grep "Image is up to date" ; then
+    docker stop con
+    docker system prune -f
+    docker run -d -p 8080:8080 --name con ziwei0/awsdevops
+fi
